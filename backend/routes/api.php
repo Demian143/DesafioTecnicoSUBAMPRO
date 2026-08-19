dde3142e0dd143b8cc5d991b114f76a4dc54ec39
Demian143 <patrickxavier140@gmail.com>
2026-08-19 05:02:16 -0300

    Feat: Rota transparencia

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TransparenciaController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('jwt.auth')->get('/me', [AuthController::class, 'me']);
+
+Route::get('/transparencia/projetos', [TransparenciaController::class, 'getTransparencia']);
