5dd9486fb5643bbed337bf81aa0722c8eda2c4d1
Demian143 <patrickxavier140@gmail.com>
2026-08-18 23:52:34 -0300

    Feat: Adição de seeding dos dados

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,24 +2,46 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use App\Models\Orgao;
+use App\Models\User;
+use App\Models\Projeto;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $data = file_get_contents(database_path('seeders/data/dados-base.json'));
+        $orgaos = json_decode($data, true);
+        $this->seedOrgaos($orgaos['orgaos']);
+        $this->seedUsers($orgaos['usuarios']);
+        $this->seedProjetos($orgaos['projetos']);
+    }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    private function seedOrgaos(array $orgaos): void
+    {
+        foreach ($orgaos as $orgaoData) {
+            Orgao::forceCreate($orgaoData);
+        }
+    }
+
+    private function seedUsers(array $users): void
+    {
+        foreach ($users as $userData) {
+            $userData['password'] = Hash::make($userData['senha_demo']);
+            unset($userData['senha_demo']);
+            User::forceCreate($userData);
+        }
+    }
+
+    private function seedProjetos(array $projetos): void
+    {
+        foreach ($projetos as $projetoData) {
+            Projeto::forceCreate($projetoData);
+        }
     }
 }
